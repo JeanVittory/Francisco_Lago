b2c890d2e391a4fa46803b489ae97a51e8bd890d
JeanVittory <jean.carjil@gmail.com>
2021-12-15 23:49:37 -0500

    empece a trabajar en la interactividad movil con js

---
+++ script.js
@@ -0,0 +1,19 @@
+const $mediumLetterContainer = document.querySelector(".medium-letter-container")
+const $mediumLetterFront = document.querySelector(".medium-letter-front")
+const $closeMenu = document.querySelector(".close-menu")
+const $mediumNavBack = document.querySelector(".medium-nav-back")
+const $smallBars = document.querySelector(".small-bars")
+
+$mediumLetterFront.addEventListener("click", show);
+$smallBars.addEventListener("click", show)
+$closeMenu.addEventListener("click", close)
+
+
+function show(){
+    $mediumNavBack.style.top = "0"
+}
+
+function close(){
+    $mediumNavBack.style.top = "-100%"
+}
+
