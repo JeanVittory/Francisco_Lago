d6b0698f85ba4bb56223361a569b246d7e648811
JeanVittory <jean.carjil@gmail.com>
2021-12-23 22:08:25 -0500

    continue con la implementacion de js en la seccion film

---
+++ views/film/scripts-film.js
@@ -0,0 +1,15 @@
+const $closeMenuFilm = document.querySelector(".close-menu-film")
+const $smallBarsFilm = document.querySelector(".small-bars-film")
+const $mobileMenuFilm = document.querySelector(".mobile-menu-film")
+
+$smallBarsFilm.addEventListener("click", show)
+$closeMenuFilm.addEventListener("click", close)
+
+
+function show(){
+    $mobileMenuFilm.style.top = "0"
+}
+
+function close(){
+    $mobileMenuFilm.style.top = "-100%"
+}
